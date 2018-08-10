1904b6879d0dc71d113dde447e7c50f56c3aa279
Harry Pujols <harrypujols@gmail.com>
2018-08-10 14:43:24 -0400

    customizing button

---
+++ scripts/menu.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+var menu = ["Start", "Quit"]
+
+func _ready():
+	
+	for item in menu:
+		var title = Label.new()
+		var menu_item = load("res://interface/menu_item.tscn")
+		var button = menu_item.instance()
+		title.set_text(item)
+		self.add_child(button)
+		button.add_child(title)
