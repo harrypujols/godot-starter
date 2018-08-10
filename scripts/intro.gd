d7d1929e90bd11387299b21c9ea8524748065e88
Harry Pujols <harrypujols@gmail.com>
2018-08-10 13:14:54 -0400

    adding array of words

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
-var title = Label.new()
 var hello = ["Hello", "world"]
 
 func _ready():
-	var hud = get_node("hud")
-	title.set_text("Hello World")
-	hud.add_child(title)
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+	var hud = get_node("hud/list")
+	
+	for word in hello:
+		var title = Label.new()
+		title.set_text(word)
+		hud.add_child(title)
