9c06f23c0ab090013629dc288eb364f57c9abfc6
Harry Pujols <harrypujols@gmail.com>
2018-08-11 16:06:28 -0400

    returning data from json

--- scripts/intro.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	var dir = Directory.new()
-	dir.copy("res://data/", "user://")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,31 +1,29 @@
 extends VBoxContainer
 
-func get_json_file(url):
+func get_data(url):
 	var file = File.new()
 	file.open(url, file.READ)
 	var text = file.get_as_text()
 	file.close()
-	var result = {}
 	var result_json = JSON.parse(text)
 	if result_json.error == OK:
-		var data = result_json.result
-		print(data)
+		var result = result_json.result
+		return(result)
 	else:
     	print("Error: ", result_json.error)
     	print("Error Line: ", result_json.error_line)
     	print("Error String: ", result_json.error_string)
-	
-var menu = ["Start", "Quit"]
 
 func _ready():
 	var url = "res://data/menu.json"
-	get_json_file(url)
+	var data = get_data(url)
+	print(data["menu"][0]["title"])
 	
-	for item in menu:
+	for item in data["menu"]:
 		var title = Label.new()
 		var menu_item = load("res://interface/menu_item.tscn")
 		var button = menu_item.instance()
-		title.set_text(item)
+		title.set_text(item["title"])
 		self.add_child(button)
 		button.add_child(title)
 		
