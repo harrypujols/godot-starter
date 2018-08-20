5bdd2a6dcf48446819b8351c36d53ef46a607de5
Harry Pujols <harrypujols@me.com>
2018-08-20 16:38:49 -0400

    top down script testing

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 export var speed = 200
 var velocity = Vector2()
-#var movement = preload("res://scripts/movement.gd")
 
 func top_down_move():
     velocity = Vector2()
@@ -15,7 +14,3 @@ func top_down_move():
     if Input.is_action_pressed('ui_up'):
         velocity.y -= 1
     velocity = velocity.normalized() * speed
-
-func _physics_process(delta):
-    top_down_move()
-    move_and_slide(velocity)
