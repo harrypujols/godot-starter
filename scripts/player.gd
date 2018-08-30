a58f1117a6320b91c4c6cde2b544127e0b855521
Harry Pujols <harrypujols@gmail.com>
2018-08-29 23:46:16 -0400

    created npc/player signal connections

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,15 @@ extends KinematicBody2D
 
 onready var player_sprite = get_node('./player_sprite')
 onready var player_animation = get_node('./player_animation')
+onready var npc = get_node('../npc/npc_dialog')
 export var speed = 200
 var velocity = Vector2()
 var direction = 'down'
 var state = 'idle'
+
+func _ready():
+	npc.connect('enter_dialog_space', self, '_on_npc_enter')
+	npc.connect('exit_dialog_space', self, '_on_npc_exit')
 
 func top_down_move():
 	velocity = Vector2()
@@ -35,6 +40,11 @@ func frame_animation():
 	
 	if player_animation.current_animation != current_animation:
 		player_animation.play(current_animation)
+
+func _on_npc_enter():
+	print('entered npc zone')
+func _on_npc_exit():
+	print('exited npc zone')
 
 func _physics_process(delta):
 	top_down_move()
