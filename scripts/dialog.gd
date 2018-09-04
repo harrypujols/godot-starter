c9c1721faf429c980e81ffa5962932f99c9878a1
Harry Pujols <harrypujols@me.com>
2018-09-04 15:58:21 -0400

    activate typing effect

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -2,22 +2,22 @@ extends Container
 
 onready var player = get_node('../../../player')
 var dialog = 'closed'
-var dialog_active = false
+var entered_dialog_zone = false
 
 func _ready():
 	player.connect('open_global_dialog', self, '_open')
 	player.connect('close_global_dialog', self, '_close')
 
 func _open():
-	dialog_active = true
+	entered_dialog_zone = true
 	print('entered dialog area')
 
 func _close():
-	dialog_active = false
+	entered_dialog_zone = false
 	print('exited dialog area')
 	
 func _input(event):
-	if Input.is_action_pressed('ui_accept') && dialog_active == true:
+	if Input.is_action_pressed('ui_accept') && entered_dialog_zone == true:
 		match dialog:
 			'open':
 				self.visible = false
@@ -27,5 +27,6 @@ func _input(event):
 				self.visible = true
 				dialog = 'open'
 				player.state = 'interact'
+				$typing_effect.start()
 			_:
 				print(dialog)
