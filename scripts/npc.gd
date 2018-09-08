ac99152ded701a466aa332e3b2e0078702f452e0
Harry Pujols <harrypujols@me.com>
2018-09-08 12:47:12 -0400

    basic dialog loop

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -2,15 +2,15 @@ extends StaticBody2D
 
 onready var dialog_box = get_node('../hud/interface/dialog')
 onready var dialog_text = dialog_box.get_node('dialog_text')
-export var character_name = 'npc'
-var dialog = ['Hello...', 'I am evil you.']
+export var character_name = 'red'
+var dialog = functions.get_json('res://data/'+ character_name +'.json')
 
 func _ready():
 	pass
 
 func _on_dialog_zone_area_entered(area):
 	global.entered_dialog_zone = true
-	dialog_text.dialog = dialog
+#	dialog_text.dialog = dialog
 	$alert_bubble.visible = true
 
 func _on_dialog_zone_area_exited(area):
