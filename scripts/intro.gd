6c4e91a4de38e30f4f031f332cf1c79cd2db20fc
Harry Pujols <harrypujols@me.com>
2018-09-10 19:40:00 -0400

    decoupling menu for reuse

---
+++ scripts/intro.gd
@@ -0,0 +1,18 @@
+extends Control
+
+onready var menu = get_node('./hud/menu')
+var data = functions.get_json('res://data/menu.json')
+
+func _ready():
+	menu.data = data
+	menu.init()
+
+func _on_menu_select(selection):
+	match selection:
+		'start':
+			get_tree().change_scene('res://rooms/start.tscn')
+		'quit':
+			get_tree().quit()
+		_:
+			print(selection)
+
