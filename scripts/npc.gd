7f6e806a69c62f0875f153ee4bc13cb09faae602
Harry Pujols <harrypujols@gmail.com>
2018-09-11 08:35:36 -0400

    cleaning up more

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-onready var dialog = get_node('../hud/interface/dialog')
+onready var dialog = get_node('../hud/dialog')
 #onready var dialog = dialog_box.get_node('dialog_text')
 export var character_name = 'red'
 var data = functions.get_json('res://data/'+ character_name +'.json')
