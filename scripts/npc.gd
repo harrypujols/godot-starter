27799ed39b35230a451c05f11a9d2db1aeccbe64
Harry Pujols <harrypujols@gmail.com>
2018-09-13 15:34:05 -0400

    trimming fat

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -5,9 +5,6 @@ export var character_name = 'character'
 var data = functions.get_json('res://data/'+ character_name +'.json')
 var next_page = 'hello'
 
-func _ready():
-	pass
-
 func _on_dialog_zone_area_entered(area):
 	global.entered_dialog_zone = true
 	$alert_bubble.visible = true
