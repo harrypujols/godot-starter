fd1378c25f1d2f1ed3ba4ad861477ec841a9e76e
Harry Pujols <harrypujols@gmail.com>
2018-09-13 00:42:48 -0400

    the mystery of using everything as buttons

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -9,63 +9,63 @@ var dialog_open = 0
 var page = 'hello'
 var current_page = page
 var next_page = page
-var dialog_text = '...'
+var dialog_text = []
+var dialog_choice = []
+var typing_effect = false
 var clicks = 0
 	
 func _input(event):
 	if Input.is_action_pressed('ui_accept') && global.entered_dialog_zone:
 		clicks += 1
-		match dialog_open:
-			1:
+		if dialog_open == 0:
+			$dialog_box.popup_centered()
+			player.state = 'interact'
+			dialog_open = 1
+			set_dialog()
+
+func set_dialog():
+	current_page = next_page
+	dialog_text.clear()
+	
+	if dialog_options.get_children().size() > 0:
+		for child in dialog_options.get_children():
+			dialog_options.remove_child(child)
+	
+	for passage in data.passages:
+		if current_page == passage.name:
+			if passage.has('choices'):
+				
+				for choice in passage.choices:
+					dialog_text.append(passage.dialog)
+					dialog_choice.append(passage.link)
+					typing_effect = false
+				
+			else:
+				dialog_text.append(passage.dialog)
+				dialog_choice.append(passage.link)
+				typing_effect = true
+				
+			for i in range(0, dialog_text.count()):
+				var menu_item = get_menu_item.instance()
+				dialog_options.add_child(menu_item)
+				var title = menu_item.get_node('title')
+				title.set_text(dialog_text[i])
+				menu_item.connect('menu_selection', self, '_on_menu_select', [menu_item.call])
+				menu_item.call = dialog_choice[i]
+				
+				if typing_effect == true:
+					title.set_visible_characters(0)
+					$dialog_box/typing_effect.start()
+					
+			dialog_options.get_child(0).grab_focus()
+				
+			if next_page == 'end':
 				$dialog_box.hide()
 				player.state = 'move'
 				dialog_open = 0
 				clicks = 0
 				current_page = page
 				next_page = page
-			0:
-				$dialog_box.popup_centered()
-				player.state = 'interact'
-				set_dialog()
-
-func set_dialog():
-	current_page = next_page
-	print('current page: ' + current_page)
-	for passage in data.passages:
-		if current_page == passage.name:
-			if passage.has('choices'):
-
-				get_node('dialog_box/dialog_text').visible = false
-				get_node('dialog_box/dialog_choices').visible = true
-
-				if dialog_options.get_children().size() > 0:
-					for child in dialog_options.get_children():
-						dialog_options.remove_child(child)
-#						child.queue_free()
-				
-				for choice in passage.choices:
-					var menu_item = get_menu_item.instance()
-					dialog_options.add_child(menu_item)
-					var title = menu_item.get_node('title')
-					title.set_text(choice.dialog)
-					menu_item.call = choice.link
-					menu_item.connect('item_focused', self, '_on_menu_select', [menu_item.call])
-		
-				dialog_options.get_child(0).grab_focus()
-#				next_page = dialog_options.get_child(0).call
-				print('next page: ' + next_page)
-				
-			else:
-				get_node('dialog_box/dialog_text').visible = true
-				get_node('dialog_box/dialog_choices').visible = false
-				dialog_text = passage.dialog
-				next_page = passage.link
-				dialog_label.set_text(dialog_text)
-				dialog_label.set_visible_characters(0)
-				$dialog_box/typing_effect.start()
-				
-			if next_page == 'end':
-				dialog_open = 1
 
 func choice_navigation():
 	if Input.is_action_pressed('ui_up'):
@@ -74,8 +74,8 @@ func choice_navigation():
 		pass
 	
 func _on_typing_effect_timeout():
-	dialog_label.set_visible_characters(dialog_label.get_visible_characters() + 1)
+	var title = dialog_options.get_child(0).get_node('title')
+	title.set_visible_characters(title.get_visible_characters() + 1)
 
-func _on_menu_select(call):
-	self.next_page = call
-	return self.next_page
+func _on_menu_select(selection):
+	next_page = selection
--- scripts/menu_item.gd
+++ scripts/menu_item.gd
@@ -2,15 +2,12 @@ extends Button
 
 var call = 'unassigned'
 signal menu_selection
-signal item_focused
 	
 func _on_menu_item_pressed():
 	emit_signal('menu_selection')
 
 func _on_menu_item_focus_entered():
 	$indicator.visible = true
-	emit_signal('item_focused')
-
 
 func _on_menu_item_focus_exited():
 	$indicator.visible = false
