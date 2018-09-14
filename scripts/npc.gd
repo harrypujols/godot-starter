915a7921fbdc42a46fd0b556e056a427ce8c3756
Harry Pujols <harrypujols@me.com>
2018-09-14 15:33:20 -0400

    setting npc vars

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,16 +1,18 @@
 extends StaticBody2D
 
 onready var dialog = get_node('../hud/dialog')
-export var character_name = 'character'
+export var character_name = 'Evil Me'
+export var file_name = 'character'
+export var dialog_entry = 'hello'
 var data = functions.get_json('res://data/'+ character_name +'.json')
-var next_page = 'hello'
+
 
 func _on_dialog_zone_area_entered(area):
 	global.entered_dialog_zone = true
 	$alert_bubble.visible = true
 	dialog.data = data
-	dialog.page = next_page
-	dialog.next_page = next_page
+	dialog.page = dialog_entry
+	dialog.next_page = dialog_entry
 
 func _on_dialog_zone_area_exited(area):
 	global.entered_dialog_zone = false
