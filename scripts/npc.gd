a7e638a66d632d79b816f5c94346ff3f66952839
Harry Pujols <harrypujols@me.com>
2018-09-14 15:38:38 -0400

    fixing var call

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -4,7 +4,7 @@ onready var dialog = get_node('../hud/dialog')
 export var character_name = 'Evil Me'
 export var file_name = 'character'
 export var dialog_entry = 'hello'
-var data = functions.get_json('res://data/'+ character_name +'.json')
+var data = functions.get_json('res://data/'+ file_name +'.json')
 
 
 func _on_dialog_zone_area_entered(area):
