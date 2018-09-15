25931b0a802239dd64d0b383bab297838cb04939
Harry Pujols <harrypujols@me.com>
2018-09-15 16:15:39 -0400

    small stuff

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 onready var dialog = get_node('../hud/dialog')
-export var character_name = 'Evil Me'
+export var character_name = 'character'
 export var file_name = 'character'
 export var dialog_entry = 'hello'
 var data = functions.get_json('res://data/'+ file_name +'.json')
