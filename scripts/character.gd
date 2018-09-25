7444b7b55df5237848b59164d896e32e15a46919
Harry Pujols <harrypujols@me.com>
2018-09-25 00:10:21 -0400

    character image loads programatically

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,10 +1,13 @@
 extends StaticBody2D
 
 onready var dialog = get_node('../hud/dialog')
-export var file_name = 'character'
+export var character_file = 'character.json'
+export var character_image = 'cat.png'
 export var dialog_entry = 'hello'
-var data = global.get_json('res://data/'+ file_name +'.json')
+var sprite = load('res://sprites/characters/' + character_image)
+var data = global.get_json('res://data/'+ character_file)
 var character_name = data.name
+var image_size
 
 
 func _on_dialog_zone_area_entered(area):
@@ -19,6 +22,8 @@ func _on_dialog_zone_area_exited(area):
 	$alert_bubble.visible = false
 	
 func _ready():
-	var image_size = $character_sprite.texture.get_size()
+	$character_sprite.set_texture(sprite)
+	image_size = $character_sprite.texture.get_size()
 	$character_shape.shape.set_extents(image_size)
-	$dialog_zone/area.shape.set_radius(4 * image_size.y)
+	$dialog_zone/area.shape.set_radius(2 * image_size.y)
+	print(image_size)
