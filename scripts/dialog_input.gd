ada62f1d2a47171c9706590fb4a35d871b16fdb2
Harry Pujols <harrypujols@me.com>
2018-10-01 19:54:53 -0400

    adding input legend controller script

--- scripts/dialog_input.gd
+++ scripts/dialog_input.gd
@@ -1,17 +1,46 @@
 extends HBoxContainer
 
+var accept_button = global.key.inverse.spacebar
+var cancel_button = global.key.inverse.delete
+
 func _ready():
-	$input_accept.input_icon = global.key.inverse.spacebar
 	$input_accept.input_text = 'accept'
 	$input_accept.text_color = 'grey'
-	
-	$input_accept.set('rect/scale', 0.5)
-	$input_accept.refresh_icon()
 	$input_accept.refresh_label()
 	
-	$input_cancel.input_icon = global.key.inverse.delete
 	$input_cancel.input_text = 'cancel'
 	$input_cancel.text_color = 'grey'
-	
-	$input_cancel.refresh_icon()
 	$input_cancel.refresh_label()
+	
+	set_icons()
+
+func set_icons():
+	$input_accept.input_icon = accept_button
+	$input_accept.refresh_icon()
+	$input_cancel.input_icon = cancel_button
+	$input_cancel.refresh_icon()
+	
+func set_keyboard_icons():
+	accept_button = global.key.inverse.spacebar
+	cancel_button = global.key.inverse.delete
+	
+func set_controller_icons():
+	if Input.is_joy_known(0):
+		match Input.get_joy_name(0):
+			'XInput Gamepad':
+				accept_button = global.button.inverse.a
+				cancel_button = global.button.inverse.b
+			'PS3 Controller', 'PS4 Controller':
+				accept_button = global.button.inverse.x
+				cancel_button = global.button.inverse.circle
+			_:
+				accept_button = global.button.inverse.b
+				cancel_button = global.button.inverse.a
+	
+func _input(event):
+	if (event is InputEventJoypadButton) or (event is InputEventJoypadMotion):
+		set_controller_icons()
+		set_icons()
+	else:
+		set_keyboard_icons()
+		set_icons()
