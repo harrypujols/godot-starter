b2f6114c9da07a61a42c9f435a17c679dd130a44
Harry Pujols <harrypujols@me.com>
2018-10-01 17:31:19 -0400

    programming parameters

--- scripts/input_ui.gd
+++ scripts/input_ui.gd
@@ -1,11 +1,21 @@
 extends HBoxContainer
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var input_icon = global.button.inverse.default
+var input_icon_separation = 16
+var input_label = Label.new()
+var input_text = 'unassigned'
+var input_text_color = global.color.white
 
 func _ready():
-	self.set('custom_constants/separation', 16)
+	self.set('custom_constants/separation', input_icon_separation)
+	$input_icon.set_texture(input_icon)
+	input_text = 'foo'
+	
+	if input_text != 'unassigned':
+		input_label.set_text(input_text)
+		input_label.name = 'input_label'
+		input_label.set('custom_colors/font_color', input_text_color)
+		self.add_child(input_label)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
