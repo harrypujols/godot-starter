ece30f54b0eb34fd74a7e458b1949ade0b92ced3
Harry Pujols <harrypujols@me.com>
2018-10-04 13:52:24 -0400

    collected item dialog

--- scripts/pickup_item.gd
+++ scripts/pickup_item.gd
@@ -4,6 +4,7 @@ onready var dialog = get_node('../../hud/dialog')
 onready var items = self.get_parent()
 export var item_name = 'item'
 export var item_image = 'interface/buttons/button.png'
+signal collected
 var sprite = load('res://sprites/' + item_image)
 var image_size
 var dialog_entry = 'dialog'
@@ -27,9 +28,11 @@ func set_item_image():
 	$item_area/item_shape.shape.set_extents(image_size)
 
 func set_item_dialog():
+	global.entered_dialog_zone = true
 	dialog.data = dialog_data
 	dialog.dialog_entry = dialog_entry
 	dialog.next_page = dialog_entry
+	dialog.clicks += 1
 	
 func _ready():
 	set_item_image()
@@ -37,11 +40,11 @@ func _ready():
 func _on_item_area_area_shape_entered(area_id, area, area_shape, self_shape):
 	items.count += 1
 	set_item_dialog()
-	dialog.clicks = 1
 	dialog.init()
+	emit_signal('collected')
 	self.queue_free()
 
 func _input(event):
-	if Input.is_action_just_pressed('ui_accept'):
+	if Input.is_action_just_pressed('ui_accept') and global.entered_dialog_zone:
 		if dialog.dialog_open == 1:
-			dialog.reset_dialog()
+			reset_dialog()
