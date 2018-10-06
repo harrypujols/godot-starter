e8d2db4ad9d1785b69783af3fed89080835750a8
Harry Pujols <harrypujols@me.com>
2018-10-06 16:35:08 -0400

    simplified conditional logic

--- scripts/characters/cat.gd
+++ scripts/characters/cat.gd
@@ -11,25 +11,13 @@ func _ready():
 	
 func _on_item_collected():
 	for passage in cat.data.passages:
-		if items.count > 0:
-			passage.call = 'items'
-		if items.count == items.total:
-			passage.call = 'success'
-		
 		if passage.name == 'items':
 			var item_name = ' ' + items.pick
 			if items.count > 1:
 				item_name = item_name + 's'
 			passage.dialog = 'You have ' + String(items.count) + item_name + '.'
-		if passage.name == 'cat' and items.count == 0:
-			passage.link = 'question'
-		if passage.name == 'cat' and items.count > 0:
-			passage.link = 'items'
 		if passage.name == 'cat' and items.count == items.total:
 			passage.link = 'success'
-		if passage.name == 'items' and items.count == items.total:
-			passage.link = 'success'
-		if passage.name == 'quest' and items.count == items.total:
-			passage.call = 'again'
-		if passage.name == 'again' and items.count == items.total:
-			passage.call = 'again'
+	
+	if items.count == items.total and cat.interactions > 0:
+		cat.dialog_entry = 'success'
