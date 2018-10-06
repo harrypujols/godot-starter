e16711a29f15868dfdbafc10365ea2e75feec9de
Harry Pujols <harrypujols@me.com>
2018-10-06 17:22:45 -0400

    space

--- scripts/characters/cat.gd
+++ scripts/characters/cat.gd
@@ -3,7 +3,6 @@ extends Node
 onready var cat = self.get_parent()
 onready var items = get_node('../../items')
 
-
 func _ready():
 	cat.character_file = 'cat.json'
 	cat.data = global.get_json('res://data/' + cat.character_file)
