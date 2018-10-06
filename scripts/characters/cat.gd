b05eca879feab2d0e966927a0ed1dd34c918323b
Harry Pujols <harrypujols@me.com>
2018-10-06 10:32:57 -0400

    staging conditionals

--- scripts/characters/cat.gd
+++ scripts/characters/cat.gd
@@ -10,4 +10,13 @@ func _ready():
 	items.connect('item_collected', self, '_on_item_collected')
 	
 func _on_item_collected():
-	print(items.count)
+	for passage in cat.data.passages:
+		if passage.name == 'items':
+			var item = ' item'
+			if items.count > 1:
+				item = ' items'
+			passage.dialog = 'You have ' + String(items.count) + item
+		if passage.name == 'cat' and items.count > 0:
+			passage.link = 'items'
+		if passage.name == 'cat' and items.count == items.total:
+			passage.link = 'success'
