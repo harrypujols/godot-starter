1c2672866eec168d1c7e602167b9b365658a49d7
Harry Pujols <harrypujols@gmail.com>
2018-10-05 23:26:56 -0400

    connecting item pickup to cat

--- characters/cat.gd
+++ characters/cat.gd
@@ -1,8 +1,13 @@
 extends Node
 
 onready var cat = self.get_parent()
+onready var items = get_node('../../items')
 
 
 func _ready():
 	cat.character_file = 'cat.json'
 	cat.data = global.get_json('res://data/' + cat.character_file)
+	items.connect('item_collected', self, '_on_item_collected')
+	
+func _on_item_collected():
+	print(items.count)
