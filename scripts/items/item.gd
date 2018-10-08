94a44189b5bd5c60bf5dc205ae75506e88689913
Harry Pujols <harrypujols@gmail.com>
2018-10-07 22:05:39 -0400

    sound?

--- scripts/items/item.gd
+++ scripts/items/item.gd
@@ -88,6 +88,7 @@ func _on_item_area_area_shape_entered(area_id, area, area_shape, self_shape):
 func _input(event):
 	if Input.is_action_just_pressed('ui_accept') and dialog_open == 1:
 		if collectible:
+			$bleep.play() 
 			collect_item()
 		dialog_open = 0
 		dialog.reset_dialog()
@@ -98,3 +99,8 @@ func _on_item_tree_exited():
 func _on_item_area_area_shape_exited(area_id, area, area_shape, self_shape):
 	dialog_open = 0
 	global.entered_dialog_zone = false
+
+
+func _on_bleep_finished():
+	$bleep.stop()
+	pass
