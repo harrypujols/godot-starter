d9ac7180ddb4358de6257ef117406b9929b2c518
Harry Pujols <harrypujols@me.com>
2018-10-11 21:41:05 -0400

    still not

--- scripts/items/spawn_point.gd
+++ scripts/items/spawn_point.gd
@@ -4,24 +4,22 @@ onready var spawn_points = get_tree().get_nodes_in_group('warps')
 onready var player = get_node('/root/stage/player')
 export var origin_name = 'point-a'
 export var target_name = 'point-b'
-var origin = self.position
+export var exit = false
+onready var origin = position
 var target 
 
 func _ready():
 	set_target()
 	print(origin)
-	print(player.position)
 
 func set_target():
-	for point in spawn_points:
-		if point.origin_name == target_name:
-			target = point.position
-		else:
-			print(point.origin_name)
+	for warp in spawn_points:
+		if warp.origin_name == target_name:
+			target = warp
 
 func _on_spawn_area_area_entered(area):
 	print('hello!')
 	if area == player.get_node('player_hitbox'):
-		if target != null:
-			player.position = target
+		if target != null and exit:
+			player.set_pos(target.position)
 		
