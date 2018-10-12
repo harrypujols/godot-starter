8f1b787103ae03158d1beb02d3bcf698a1345654
Harry Pujols <harrypujols@me.com>
2018-10-12 19:45:22 -0400

    setting unlockable region

--- scripts/items/unlockable.gd
+++ scripts/items/unlockable.gd
@@ -1,7 +1,22 @@
 extends Node
 
 onready var item = get_parent()
+onready var item_sprite = item.get_node('item_sprite')
+onready var item_area = item.get_node('item_area')
+var shape = CircleShape2D.new()
+var collision = CollisionShape2D.new()
 
 func _ready():
 	item.dialog_active = false
 	item.item_image = 'items/chest.png'
+	set_unlockable_area()
+	
+func set_unlockable_area():
+	var image_size = item_sprite.texture.get_size()
+	shape.set_radius(2 * image_size.y)
+	
+	for child in item_area.get_children():
+		child.queue_free()
+		
+	collision.set_shape(shape)
+	item_area.add_child(collision)
