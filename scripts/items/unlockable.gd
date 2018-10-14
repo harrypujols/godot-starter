5c5c8439c0461f8d40684137eb9353dfebb1af0b
Harry Pujols <harrypujols@me.com>
2018-10-14 12:51:23 -0400

    created some sort of key holding system

--- scripts/items/unlockable.gd
+++ scripts/items/unlockable.gd
@@ -5,15 +5,20 @@ onready var item_sprite = item.get_node('item_sprite')
 onready var item_area = item.get_node('item_area')
 var shape = CircleShape2D.new()
 var collision = CollisionShape2D.new()
+var key_pressed = false
 
 func _ready():
 	item.dialog_active = false
 	set_unlockable_area()
 
 func _input(event):
-	if Input.is_action_pressed('ui_accept') and item.entered_dialog_zone and event.echo:
-		print('the key is pressed down')
-			
+	if Input.is_action_pressed('ui_accept') and item.entered_dialog_zone:
+		key_pressed = true
+		$key_charge.start()
+	else:
+		key_pressed = false
+		$key_charge.stop()
+		
 func set_unlockable_area():
 	var image_size = item_sprite.texture.get_size()
 	shape.set_radius(2 * image_size.y)
@@ -23,3 +28,8 @@ func set_unlockable_area():
 		
 	collision.set_shape(shape)
 	item_area.add_child(collision)
+
+func _on_key_charge_timeout():
+	print('timeout')
+	if key_pressed:
+		print('ding ding!')
