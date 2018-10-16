193eaea38b330ce1a8499837192f9690b541c030
Harry Pujols <harrypujols@me.com>
2018-10-15 22:30:00 -0400

    fix

--- scripts/items/unlockable.gd
+++ scripts/items/unlockable.gd
@@ -35,7 +35,7 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	for i in range(nb_points+1):
 		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-		draw_polygon(points_arc, colors)
+	draw_polygon(points_arc, colors)
 
 func _draw():
 	var center = get_size() / 2
