c82646c237048f3bfbf90740f99963e7eb6ea07e
Harry Pujols <harrypujols@me.com>
2018-10-15 22:13:36 -0400

    no circle

--- scripts/items/unlockable.gd
+++ scripts/items/unlockable.gd
@@ -28,38 +28,6 @@ func set_unlockable_area():
 		
 	collision.set_shape(shape)
 	item_area.add_child(collision)
-
-func draw_circle_arc(center, radius, angle_from, angle_to, color):
-    var nb_points = 32
-    var points_arc = PoolVector2Array()
-
-    for i in range(nb_points+1):
-        var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
-        points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-
-    for index_point in range(nb_points):
-        draw_line(points_arc[index_point], points_arc[index_point + 1], color)
-
-func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
-	var nb_points = 32
-	var points_arc = PoolVector2Array()
-	points_arc.push_back(center)
-	var colors = PoolColorArray([color])
-	
-	for i in range(nb_points+1):
-		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
-		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-	
-	draw_polygon(points_arc, colors)
-
-func _draw():
-	var center = item.position
-	var radius = 80
-	var angle_from = 75
-	var angle_to = 195
-	var color = Color(1.0, 0.0, 0.0)
-	draw_circle_arc(center, radius, angle_from, angle_to, color)
-	draw_circle_arc_poly(center, radius, angle_from, angle_to, color)
 	
 func _process(delta):
 	if key_pressed:
