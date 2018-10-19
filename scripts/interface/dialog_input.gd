5398e73a73ca3e66a4338252e67e6e41a96c8a23
Harry Pujols <harrypujols@me.com>
2018-10-19 19:37:57 -0400

    resizable

--- scripts/interface/dialog_input.gd
+++ scripts/interface/dialog_input.gd
@@ -2,21 +2,11 @@ extends HBoxContainer
 
 func _ready():
 	$accept.text_color = 'grey'
+	$accept.font_size = 12
+	$accept.icon_size = 18
 	$accept.refresh_label()
 	
 	$cancel.text_color = 'grey'
+	$cancel.font_size = 12
+	$cancel.icon_size = 18
 	$cancel.refresh_label()
-	
-#	set_icons()
-	
-#	self.rect_scale = Vector2(.6, .6)
-
-#func set_icons():
-#	input.icon_color = 'light'
-#	$input_accept.input_icon = input.input_accept
-#	$input_accept.refresh_icon()
-#	$input_cancel.input_icon = input.input_cancel
-#	$input_cancel.refresh_icon()
-#
-#func _input(event):
-#	set_icons()
--- scripts/interface/input_ui.gd
+++ scripts/interface/input_ui.gd
@@ -2,7 +2,7 @@ extends HBoxContainer
 
 export(String) var input_text
 export var text_color = 'white'
-var input_text_color
+var font_size
 var input_icon_separation = 16
 var input_icon
 var icon_color
@@ -12,13 +12,15 @@ func refresh_label():
 	$input_label.visible = true
 	$input_label.set_text(input_text)
 	$input_label.set('custom_colors/font_color', global.color[text_color])
+	if font_size:
+		$input_label.set('custom_fonts/settings/size', font_size)
 	
 func refresh_icon():
 	$input_icon.set_text(input_icon)
 	if icon_color:
 		$input_icon.set('custom_colors/font_color', global.color[icon_color])
 	if icon_size:
-		$input_icon.get('custom_fonts/font').set_size(icon_size)
+		$input_icon.set('custom_fonts/settings/size', icon_size)
 	
 func _ready():
 	self.set('custom_constants/separation', input_icon_separation)
