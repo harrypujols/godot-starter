0dd64e343095351d750ebef137aee8a5af8c6159
Harry Pujols <harrypujols@me.com>
2018-10-19 17:39:53 -0400

    deeper icon rabbit hole

--- scripts/interface/input_ui.gd
+++ scripts/interface/input_ui.gd
@@ -4,25 +4,21 @@ export(String) var input_text
 export var text_color = 'white'
 var input_text_color
 var input_icon_separation = 16
-var input_label = Label.new()
 var input_icon
 var icon_color
+var icon_size
 
 func refresh_label():
-	for child in self.get_children():
-		if child is Label:
-			child.queue_free()
-			self.remove_child(child)
-	
-	input_label.set_text(input_text)
-	input_label.name = 'input_label'
-	input_label.set('custom_colors/font_color', global.color[text_color])
-	self.add_child(input_label)
+	$input_label.visible = true
+	$input_label.set_text(input_text)
+	$input_label.set('custom_colors/font_color', global.color[text_color])
 	
 func refresh_icon():
 	$input_icon.set_text(input_icon)
 	if icon_color:
 		$input_icon.set('custom_colors/font_color', global.color[icon_color])
+	if icon_size:
+		$input_icon.get('custom_fonts/font').set_size(icon_size)
 	
 func _ready():
 	self.set('custom_constants/separation', input_icon_separation)
@@ -32,3 +28,5 @@ func _ready():
 	
 	if input_text:
 		refresh_label()
+	else:
+		$input_label.visible = false
