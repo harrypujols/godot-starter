27b8e4d9dffeb70c9b70ed0e8b877137627b2292
Harry Pujols <harrypujols@me.com>
2018-10-19 19:58:24 -0400

    icons changeable from ui

--- scripts/interface/input_ui.gd
+++ scripts/interface/input_ui.gd
@@ -2,31 +2,34 @@ extends HBoxContainer
 
 export(String) var input_text
 export var text_color = 'white'
-var font_size
-var input_icon_separation = 16
-var input_icon
-var icon_color
-var icon_size
+export(String) var icon_color = 'white'
+export var icon_separation = 16
+export(int) var font_size
+export(int) var icon_size
+var icon
+
+
 
 func refresh_label():
 	$input_label.visible = true
 	$input_label.set_text(input_text)
 	$input_label.set('custom_colors/font_color', global.color[text_color])
+	
 	if font_size:
 		$input_label.set('custom_fonts/settings/size', font_size)
 	
 func refresh_icon():
-	$input_icon.set_text(input_icon)
-	if icon_color:
-		$input_icon.set('custom_colors/font_color', global.color[icon_color])
+	$input_icon.set('custom_colors/font_color', global.color[icon_color])
+	
+	if icon:
+		$input_icon.set_text(icon)
 	if icon_size:
 		$input_icon.set('custom_fonts/settings/size', icon_size)
 	
 func _ready():
-	self.set('custom_constants/separation', input_icon_separation)
+	self.set('custom_constants/separation', icon_separation)
 	
-	if input_icon:
-		refresh_icon()
+	refresh_icon()
 	
 	if input_text:
 		refresh_label()
