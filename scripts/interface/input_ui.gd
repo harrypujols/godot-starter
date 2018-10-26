61e7929ddaffbceef8d5bb8401da77f2719a8124
Harry Pujols <harrypujols@me.com>
2018-10-26 11:16:45 -0400

    icon size workaround

--- scripts/interface/input_ui.gd
+++ scripts/interface/input_ui.gd
@@ -5,12 +5,18 @@ export var text_color = 'white'
 export var icon_color = 'white'
 export var icon_separation = 16
 export(int) var font_size
-export(int) var icon_size
+export(String, 'x-small', 'small', 'medium', 'large') var icon_size = 'medium'
 export(String, 'default', 'accept', 'cancel') var icon_name = 'default'
 onready var icon = find_node('icon')
 onready var label = find_node('label')
 onready var wrapper = find_node('wrapper')
 onready var input_icons = find_node('input_icons')
+var icon_font = {
+		'x-small': load('res://fonts/icons_xs.tres'),
+		'small': load('res://fonts/icons_s.tres'),
+		'medium': load('res://fonts/icons_m.tres'),
+		'large': load('res://fonts/icons_l.tres')
+	}
 
 func refresh_label():
 	if input_text:
@@ -26,9 +32,10 @@ func refresh_label():
 func refresh_icon(icon_name):
 	icon.set('custom_colors/font_color', global.color[icon_color])
 	icon.set_text(input_icons[icon_name])
+	icon.add_font_override('font', icon_font[icon_size])
 	
-	if icon_size:
-		icon.get_font('font').size = icon_size
+#	if icon_size:
+#		icon.get_font('font').size = icon_size
 		
 func set_separation(separation):
 	wrapper.set('custom_constants/separation', separation)
