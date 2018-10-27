e7ec0f3eeb2cb2e5bd3080c142c1a12712a412e6
Harry Pujols <harrypujols@me.com>
2018-10-27 13:00:43 -0400

    very buggy interpolation

--- scripts/characters/cat.gd
+++ scripts/characters/cat.gd
@@ -5,24 +5,30 @@ onready var items = get_node('/root/stage/items')
 
 signal key_acquired
 
+func interpolate_data():
+	var item_name = items.item_name + 's'
+	var count = 'no'
+	
+	for passage in cat.data.passages:
+		if passage.name == 'question' or passage.name == 'success':
+			passage.dialog = passage.dialog.format({'items': item_name})
+			
+		if passage.name == 'items':
+			if items.count > 0:
+				count = String(items.count)
+			if items.count == 1:
+				item_name = items.item_name
+			else:
+				item_name = items.item_name + 's'
+			passage.dialog = passage.dialog.format({'count': count, 'items': item_name})
+
 func _ready():
 	cat.connect('interacted', self, '_on_cat_interaction')
 	items.connect('item_collected', self, '_on_item_collected')
 
 func _on_item_collected():
-	for passage in cat.data.passages:
-#		if passage.name == 'question' or passage.name == 'success':
-#			var item_name = items.pick + 's'
-#			passage.dialog = passage.dialog % item_name
-			
-		if passage.name == 'items':
-			var item_name = items.pick
-			if not items.count == 1:
-				item_name = item_name + 's'
-			passage.dialog = passage.dialog.format({'count': String(items.count), 'items': item_name})
-		
-		if passage.name == 'cat' and items.count == items.total:
-			passage.link = 'success'
+	print(items.count)
+	interpolate_data()
 
 	if items.count == items.total and cat.interactions > 0:
 		cat.dialog_entry = 'success'
@@ -30,3 +36,6 @@ func _on_item_collected():
 func _on_cat_interaction():
 	if items.count == items.total and cat.dialog_entry == 'again':
 		emit_signal('key_acquired')
+
+func _on_dialog_zone_area_entered(area):
+	interpolate_data()
--- scripts/items/pickup_items.gd
+++ scripts/items/pickup_items.gd
@@ -3,17 +3,17 @@ extends Node
 onready var coins = get_tree().get_nodes_in_group('coins')
 var total
 var count = 0
-var pick = 'item'
+var item_name
 
 signal item_collected
 
 func _ready():
 	total = coins.size()
 	for coin in coins:
-		coin.connect('collected', self, '_on_item_collected', [coin.item_name])
+		coin.connect('collected', self, '_on_item_collected')
+		item_name = coin.item_name
 
-func _on_item_collected(item):
+func _on_item_collected():
 	$bleep.play('blop')
 	count += 1
-	pick = item
 	emit_signal('item_collected')
